6a122481ac1d00abb4374e1d7ae312c76065149c
Fea Hsu <fea.x.cn@gmail.com>
2021-06-28 18:35:09 +0800

    nothing

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,27 @@
 <template>
-  <div class="text-red-500">
-    你永远是我的{{name}}，{{name.repeat(2)}}，{{name.repeat(2)}}！
+  <div class="container mx-auto">
+    <div class="flex flex-wrap bg-gray-100">
+      <div class="flex flex-col w-2/4 flex-none p-2 space-y-2">
+        <div class="flex items-center h-20 rounded-lg bg-pink-600">
+          <div class="w-full">1</div>
+        </div>
+        <div class="flex items-center h-20 rounded-lg bg-red-300">
+          <div class="w-full">1</div>
+        </div>
+      </div>
+      <div class="w-2/4 flex-none p-2">
+        <div class="h-20 rounded-lg bg-opacity-100 bg-green-600">2</div>
+      </div>
+      <div class="w-2/4 flex-none p-2">
+        <div class="h-16 rounded-lg bg-opacity-100 bg-indigo-600">3</div>
+      </div>
+    </div>
+    <div class="text-red-100">
+      <!-- 你永远是我的{{name}}，{{name.repeat(2)}}，{{name.repeat(2)}}！ -->
+      <div class="box-content h-32 w-32 p-4 border-4 ...">
+        hoo
+      </div>
+    </div>
   </div>
 </template>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Other from '@/pages/Other.vue';
 
 const About = { template: '<h1>About</h1>' };
@@ -13,6 +13,6 @@ const routes = [
 ];
 
 export default createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
